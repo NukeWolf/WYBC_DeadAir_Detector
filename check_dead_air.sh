3c32ecf3ef54affea6c2922de43b7ee1fde3493e
whyalex <smart.alex.huang@gmail.com>
2022-11-25 10:04:26 -0500

    Added Cooldown to alerts

--- check_dead_air.sh
+++ check_dead_air.sh
@@ -10,11 +10,23 @@ then
     chmod +x ./aacgain
 fi
 
+# Don't Repeat Notifications once every 6 hours.
+MIN_TIME_BETWEEN_ALERTS=21600
+PREV_DATE=$(tail -n 1 < log)+${MIN_TIME_BETWEEN_ALERTS}
+
+if [[ ${PREV_DATE} -gt $(date +%s) ]]
+then
+exit
+fi
+
+
+
 
 #Global Variables
 SLEEP_TIMEOUT=15
-DEAD_AIR_TRIGGER=200
+DEAD_AIR_TRIGGER=500
 #White noise is around 60
+#Music should get abouve 10,000 in 15 seconds.
 
 #Download Audio Stream
 curl -o stream ${STREAM_LINK} & 
@@ -26,11 +38,13 @@ curl -o stream ${STREAM_LINK} &
 
    #Gets the max amplitude
    MAX_AMPLITUDE=$(./aacgain -o stream | awk -F '\t' '{print $4}' | head -2 | tail -n 1)
+   echo ${MAX_AMPLITUDE}
    #Checks if the amplitude is bigger than some treshhold
    if [[ 1 -eq $(echo "${DEAD_AIR_TRIGGER} > ${MAX_AMPLITUDE}" | bc) ]]
    then
     date >> log
     echo ${MAX_AMPLITUDE} >> log
+    date +%s >> log
     #Send API Request to project in server
     curl ${GOOGLE_SCRIPTS_API_LINK}
    fi
